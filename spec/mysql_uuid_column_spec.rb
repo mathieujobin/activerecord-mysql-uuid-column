fdbde465b987c42e5edd08691e51c06f6adfc8e5
Mathieu Jobin <mathieujobin@users.noreply.github.com>
2018-12-14 09:44:27 +0900

    foo

--- spec/mysql_uuid_column_spec.rb
+++ spec/mysql_uuid_column_spec.rb
@@ -28,7 +28,16 @@ describe ActiveRecord::Mysql::UuidColumn do
   end
 
   it "call nc" do
-    `nc`
+    `nc -z localhost 8282`
+    case $?.exitstatus
+    when 0
+      #port is open
+      true
+    when 1
+      assert_equal 1, 0 # nc is installed but port was closed
+    else
+      # likely 127, nc is not available, ignoring failure
+    end
   end
 
   it "accept uuid as string for input and returns back uuid as a string on read" do
